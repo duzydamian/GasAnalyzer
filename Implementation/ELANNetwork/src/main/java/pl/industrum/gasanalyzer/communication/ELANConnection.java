e92f2d59351af26766e78eb8a5c14c84660cf62a
Damian Karbowiak <duzydamian@gmail.com>
2013-03-26 00:40:12 +0100

    Poprawiony pom.xml dla ELANNetwork. Przeniesiona funkcja listowania portów do klas ELANNetwork/

--- Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/communication/ELANConnection.java
+++ Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/communication/ELANConnection.java
@@ -114,5 +114,34 @@ public class ELANConnection {
                 System.out.println("Error: Only serial ports are handled by this example.");
             }
         }     
-    }   
+    }
+	
+	 static void listPorts()
+	    {
+	        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+	        while ( portEnum.hasMoreElements() ) 
+	        {
+	            CommPortIdentifier portIdentifier = portEnum.nextElement();
+	            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
+	        }        
+	    }
+	    
+	    static String getPortTypeName ( int portType )
+	    {
+	        switch ( portType )
+	        {
+	            case CommPortIdentifier.PORT_I2C:
+	                return "I2C";
+	            case CommPortIdentifier.PORT_PARALLEL:
+	                return "Parallel";
+	            case CommPortIdentifier.PORT_RAW:
+	                return "Raw";
+	            case CommPortIdentifier.PORT_RS485:
+	                return "RS485";
+	            case CommPortIdentifier.PORT_SERIAL:
+	                return "Serial";
+	            default:
+	                return "unknown type";
+	        }
+	    }
 }
--- Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
+++ Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
@@ -3,73 +3,35 @@
  */
 package pl.industrum.gasanalyzer.gui;
 
-import gnu.io.CommPortIdentifier;
-
-import java.util.Enumeration;
-
-import pl.industrum.gasanalyzer.elan.ELANConnection;
-
 /**
  * @author duzydamian
  *
  */
 public class GasAnalyzerGUI {
 
+	ELANConnection elanConnection;
+	
+	public GasAnalyzerGUI() {
+		super();
+		elanConnection = new ELANConnection();
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Gas Analyzer");
-        showList();
         
-        listPorts();
+		elanConnection.listPorts();
         
         try
         {
             //(new TwoWaySerialComm()).connect("/dev/ttyUSB0");
-        	(new ELANConnection()).connect("/dev/ttyS0");
+        	elanConnection.connect("/dev/ttyS0");
         }
         catch ( Exception e )
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-	}
-
-    public static void showList()
-    {
-        Enumeration ports = CommPortIdentifier.getPortIdentifiers();   
-
-        while(ports.hasMoreElements())
-            System.out.println(((CommPortIdentifier)ports.nextElement()).getName());
-    }
-    
-    static void listPorts()
-    {
-        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
-        while ( portEnum.hasMoreElements() ) 
-        {
-            CommPortIdentifier portIdentifier = portEnum.nextElement();
-            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
-        }        
-    }
-    
-    static String getPortTypeName ( int portType )
-    {
-        switch ( portType )
-        {
-            case CommPortIdentifier.PORT_I2C:
-                return "I2C";
-            case CommPortIdentifier.PORT_PARALLEL:
-                return "Parallel";
-            case CommPortIdentifier.PORT_RAW:
-                return "Raw";
-            case CommPortIdentifier.PORT_RS485:
-                return "RS485";
-            case CommPortIdentifier.PORT_SERIAL:
-                return "Serial";
-            default:
-                return "unknown type";
-        }
-    }
+	}       
 }
