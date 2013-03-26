542fe3835bd0cd5f59e95fbae6305c878a7fe796
Damian Karbowiak <duzydamian@gmail.com>
2013-03-26 14:32:36 +0100

    Test commit na własny branch

--- Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
+++ Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
@@ -21,6 +21,8 @@ public class GasAnalyzerGUI {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Gas Analyzer");
+		System.out.println("Teoretycznie proste GUI");
+		
         
 		ELANConnection elanConnection;
 		elanConnection = new ELANConnection();
