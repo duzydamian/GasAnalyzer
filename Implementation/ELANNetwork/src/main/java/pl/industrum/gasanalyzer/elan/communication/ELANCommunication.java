6a4dc54028762ec69c88c53668435848f67d327c
Damian Karbowiak <duzydamian@gmail.com>
2013-03-27 18:40:13 +0100

    Wydzieleni klasy Singleton dla połączenia oraz klasy dla komunikacji w obrębie połączenia.

--- Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANCommunication.java
+++ Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANCommunication.java
@@ -13,6 +13,34 @@ public class ELANCommunication {
 	
 	public ELANCommunication() {
 		super();
-		elanConnection = new ELANConnection();
+		elanConnection = ELANConnection.getInstance();
+	}
+	
+	public void readFrame(){
+    	System.out.println("Read frame from port");
+    	int previousCharacter = -1;
+    	//Read first character from new frame
+    	int courentCharacter = elanConnection.read();
+    	while(courentCharacter!=-1){
+    		String frame = "";
+    		//Add current character to collected frame
+    		frame += Integer.toHexString(courentCharacter)+"H ";
+    		do{   
+    			previousCharacter = courentCharacter;  
+    			//Read next character from new frame
+    			courentCharacter = elanConnection.read();	
+    			//Add current character to collected frame
+    			frame += Integer.toHexString(courentCharacter)+"H "; 
+    		} while (!((previousCharacter==16) & (courentCharacter==3)));
+    		//Add CRC16 to frame
+			frame += Integer.toHexString(elanConnection.read())+"H "+Integer.toHexString(elanConnection.read())+"H ";
+    		System.out.println(frame);
+    		//Read first character from next frame 
+    		courentCharacter = elanConnection.read();
+    	}
+	}
+	
+	public void writeFrame(){
+		
 	}
 }
--- Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANConnection.java
+++ Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANConnection.java
@@ -20,6 +20,11 @@ import java.util.Vector;
 public class ELANConnection {
 	
 	/**
+	 * Instance for singleton
+	 */
+	private static ELANConnection instance = null;
+	
+	/**
 	 * Interface Parameters
 	 * Level RS485
 	 */
@@ -28,151 +33,157 @@ public class ELANConnection {
 	static int ELAN_STOP_BITS = SerialPort.STOPBITS_1;
 	static int ELAN_PARITY = SerialPort.PARITY_NONE;
 
+	BufferedReader is; 
+	PrintStream os;
+	SerialPort serialPort;
+			
+	protected ELANConnection() {
+		super();
+		//
+		// Open the input Reader and output stream. The choice of a
+		// Reader and Stream are arbitrary and need to be adapted to
+		// the actual application. Typically one would use Streams in
+		// both directions, since they allow for binary data transfer,
+		// not only character data transfer.
+		//
+		is = null; // for demo purposes only. A stream would be more typical.
+		os = null;
+	}	
 	
-	public void connect ( String portName ) throws Exception
-    {
-        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-        if ( portIdentifier.isCurrentlyOwned() )
-        {
-            System.out.println("Error: Port is currently in use");
-        }
-        else
-        {
-            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);            
-            
-            if ( commPort instanceof SerialPort )
-            {
-            	System.out.println("Port " + portName + " successfull open.");
-                SerialPort serialPort = (SerialPort) commPort;
-               // serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
-                
-                serialPort.setSerialPortParams(
-                		ELAN_BAUD_RATE,
-                	    ELAN_DATA_BITS,
-                	    ELAN_STOP_BITS,
-                	    ELAN_PARITY);
-
-                	//
-                	// Open the input Reader and output stream. The choice of a
-                	// Reader and Stream are arbitrary and need to be adapted to
-                	// the actual application. Typically one would use Streams in
-                	// both directions, since they allow for binary data transfer,
-                	// not only character data transfer.
-                	//
-                	BufferedReader is = null;  // for demo purposes only. A stream would be more typical.
-                	PrintStream    os = null;
-
-                	try {
-                	  is = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-                	} catch (IOException e) {
-                	  System.err.println("Can't open input stream: write-only");
-                	  is = null;
-                	}
-
-                	//
-                	// New Linux systems rely on Unicode, so it might be necessary to
-                	// specify the encoding scheme to be used. Typically this should
-                	// be US-ASCII (7 bit communication), or ISO Latin 1 (8 bit
-                	// communication), as there is likely no modem out there accepting
-                	// Unicode for its commands. An example to specify the encoding
-                	// would look like:
-                	//
-//                	     os = new PrintStream(port.getOutputStream(), true, "ISO-8859-1");
-                	//
-                	os = new PrintStream(serialPort.getOutputStream(), true);
-
-                	  
-                	//
-                	// Actual data communication would happen here
-                	// performReadWriteCode();
-                	//
-
-                	// Read the response
-                	//String response = is.readLine(); // if you sent "AT" then response == "OK"
-                	//System.out.println(response);
-                	
-                	//System.out.println("Write to port");  
-                	//os.print("test");                	
-
-                	System.out.println("Read frame from port");
-                	int previousCharacter = -1;
-                	//Read first character from new frame
-                	int courentCharacter = is.read();
-                	while(courentCharacter!=-1){
-                		String frame = "";
-                		//Add current character to collected frame
-                		frame += Integer.toHexString(courentCharacter)+"H ";
-                		do{   
-                			previousCharacter = courentCharacter;  
-                			//Read next character from new frame
-                			courentCharacter = is.read();	
-                			//Add current character to collected frame
-                			frame += Integer.toHexString(courentCharacter)+"H "; 
-                		} while (!((previousCharacter==16) & (courentCharacter==3)));
-                		//Add CRC16 to frame
-            			frame += Integer.toHexString(is.read())+"H "+Integer.toHexString(is.read())+"H ";
-                		System.out.println(frame);
-                		//Read first character from next frame 
-                		courentCharacter = is.read();
-                	}
-                	
-                	
-                	//
-                	// It is very important to close input and output streams as well
-                	// as the port. Otherwise Java, driver and OS resources are not released.
-                	//
-                	if (is != null) is.close();
-                	if (os != null) os.close();
-                	if (serialPort != null) serialPort.close();
-
-            }
-            else
-            {
-                System.out.println("Error: Only serial ports are handled by this example.");
-            }
-        }     
-    }
-	
-	@SuppressWarnings("unchecked")
-	public Vector<String> vectorPorts(){
-		
-		Vector<String> ports = new Vector<String>();
-		java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
-        while ( portEnum.hasMoreElements() ) 
-        {
-            CommPortIdentifier portIdentifier = portEnum.nextElement();
-            ports.add(portIdentifier.getName());            
-        }  
-		return ports;		
+	public synchronized static ELANConnection getInstance() {
+		if (instance == null) {
+			instance = new ELANConnection();
+		}
+		return instance;
 	}
 	
-	 @SuppressWarnings("unchecked")
-	public void listPorts()
-	    {
-	        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
-	        while ( portEnum.hasMoreElements() ) 
-	        {
-	            CommPortIdentifier portIdentifier = portEnum.nextElement();
-	            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
-	        }        
-	    }
+	public void connect(String portName) throws Exception {
+		CommPortIdentifier portIdentifier = CommPortIdentifier
+				.getPortIdentifier(portName);
+		if (portIdentifier.isCurrentlyOwned()) {
+			System.out.println("Error: Port is currently in use");
+		} else {
+			CommPort commPort = portIdentifier.open(this.getClass().getName(),
+					2000);
+
+			if (commPort instanceof SerialPort) {
+				System.out.println("Port " + portName + " successfull open.");
+				serialPort = (SerialPort) commPort;
+
+				serialPort.setSerialPortParams(ELAN_BAUD_RATE, ELAN_DATA_BITS,
+						ELAN_STOP_BITS, ELAN_PARITY);
+
+				try {
+					is = new BufferedReader(new InputStreamReader(
+							serialPort.getInputStream()));
+				} catch (IOException e) {
+					System.err.println("Can't open input stream: write-only");
+					is = null;
+				}
+
+				//
+				// New Linux systems rely on Unicode, so it might be necessary
+				// to
+				// specify the encoding scheme to be used. Typically this should
+				// be US-ASCII (7 bit communication), or ISO Latin 1 (8 bit
+				// communication), as there is likely no modem out there
+				// accepting
+				// Unicode for its commands. An example to specify the encoding
+				// would look like:
+				//
+				// os = new PrintStream(port.getOutputStream(), true,
+				// "ISO-8859-1");
+				//
+				os = new PrintStream(serialPort.getOutputStream(), true);
+
+				//
+				// Actual data communication would happen here
+				// performReadWriteCode();
+				//
+
+				// Read the response
+				// String response = is.readLine(); // if you sent "AT" then
+				// response == "OK"
+				// System.out.println(response);
+
+				// System.out.println("Write to port");
+				// os.print("test");
+			} else {
+				System.out
+						.println("Error: Only serial ports are handled by this example.");
+			}
+		}
+	}
+
+	public void disconnect() {
+		//
+		// It is very important to close input and output streams as well
+		// as the port. Otherwise Java, driver and OS resources are not
+		// released.
+		//
+		try {
+			if (is != null)
+				is.close();
+			if (os != null)
+				os.close();
+			if (serialPort != null)
+				serialPort.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int read() {
+		try {
+			return is.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return -2;
+	}
+
+	public void write(int byteToWrite) {
+		os.write(byteToWrite);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Vector<String> vectorPorts() {
+
+		Vector<String> ports = new Vector<String>();
+		java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier
+				.getPortIdentifiers();
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier portIdentifier = portEnum.nextElement();
+			ports.add(portIdentifier.getName());
+		}
+		return ports;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void listPorts() {
+		java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier
+				.getPortIdentifiers();
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier portIdentifier = portEnum.nextElement();
+			System.out.println(portIdentifier.getName() + " - "
+					+ getPortTypeName(portIdentifier.getPortType()));
+		}
+	}
 	    
-	    static String getPortTypeName ( int portType )
-	    {
-	        switch ( portType )
-	        {
-	            case CommPortIdentifier.PORT_I2C:
-	                return "I2C";
-	            case CommPortIdentifier.PORT_PARALLEL:
-	                return "Parallel";
-	            case CommPortIdentifier.PORT_RAW:
-	                return "Raw";
-	            case CommPortIdentifier.PORT_RS485:
-	                return "RS485";
-	            case CommPortIdentifier.PORT_SERIAL:
-	                return "Serial";
-	            default:
-	                return "unknown type";
-	        }
-	    }
+	static String getPortTypeName(int portType) {
+		switch (portType) {
+		case CommPortIdentifier.PORT_I2C:
+			return "I2C";
+		case CommPortIdentifier.PORT_PARALLEL:
+			return "Parallel";
+		case CommPortIdentifier.PORT_RAW:
+			return "Raw";
+		case CommPortIdentifier.PORT_RS485:
+			return "RS485";
+		case CommPortIdentifier.PORT_SERIAL:
+			return "Serial";
+		default:
+			return "unknown type";
+		}
+	}
 }
