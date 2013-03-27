5a0287520eea8b46140f3b7696f6f35dd663422f
Damian Karbowiak <duzydamian@gmail.com>
2013-03-27 10:51:06 +0100

    Dodanie zależności dla SWT. Utworzenie pierwszego okienka, które jeszcze nic nie robi.

--- Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
+++ Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerGUI.java
@@ -3,43 +3,27 @@
  */
 package pl.industrum.gasanalyzer.gui;
 
-import pl.industrum.gasanalyzer.elan.communication.ELANConnection;
 
 /**
  * @author duzydamian(Damian Karbowiak)
  *
  */
-public class GasAnalyzerGUI {
-
-	
+public class GasAnalyzerGUI {	
 	
 	public GasAnalyzerGUI() {
 		super();		
 	}
+	
 	/**
+	 * Launch the application.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Gas Analyzer");
-		System.out.println("Teoretycznie proste GUI");
-		System.out.println(" ^_^ ");
-		System.out.println(" O_o ");
-		
-        
-		ELANConnection elanConnection;
-		elanConnection = new ELANConnection();
-		
-		elanConnection.listPorts();
-        
-        try
-        {
-        	elanConnection.connect("/dev/ttyUSB0");
-        	//elanConnection.connect("/dev/ttyS0");
-        }
-        catch ( Exception e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+		try {
+			GasAnalyzerMainWindow window = new GasAnalyzerMainWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		       
 	}       
 }
