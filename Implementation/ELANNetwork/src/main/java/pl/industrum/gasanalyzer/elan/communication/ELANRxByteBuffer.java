d5d6ea2937ad6a5c3144c7c03d88ccbb0c01e2b4
Damian Karbowiak <duzydamian@gmail.com>
2013-04-04 12:51:36 +0200

    Repair observator classes.

--- Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANRxByteBuffer.java
+++ Implementation/ELANNetwork/src/main/java/pl/industrum/gasanalyzer/elan/communication/ELANRxByteBuffer.java
@@ -1,56 +1,35 @@
 package pl.industrum.gasanalyzer.elan.communication;
 
-import java.io.BufferedReader;
+import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Queue;
-import java.util.LinkedList;
 
 public class ELANRxByteBuffer extends Observable implements Runnable
-{
-	private BufferedReader is;
+{	
 	private Queue<Integer> dataBuffer;
+	ELANCommunication communication;
 	
-	public ELANRxByteBuffer( BufferedReader is )
+	public ELANRxByteBuffer()
 	{
-		this.is = is;
 		dataBuffer = new LinkedList<Integer>();
 	}
 	
 	public void run()
 	{
+		communication = new ELANCommunication();
 		while( !Thread.currentThread().isInterrupted() )
 		{
 			try
-			{
-				int previousCharacter = -1;
-				//Read first character from new frame
-				int currentCharacter = is.read();   
-				
-	        	while( currentCharacter != -1 )
-	        	{
-	        		dataBuffer.add( currentCharacter );
-	        		String frame = "";
-	        		//Add current character to collected frame
-	        		frame += Integer.toHexString(currentCharacter)+"H ";
-	        		do{   
-	        			previousCharacter = currentCharacter;  
-	        			//Read next character from new frame
-	        			currentCharacter = is.read();	
-	        			//Add current character to collected frame
-	        			frame += Integer.toHexString(currentCharacter)+"H "; 
-	        		} while (!((previousCharacter==16) & (currentCharacter==3)));
-	        		//Add CRC16 to frame
-	    			frame += Integer.toHexString(is.read())+"H "+Integer.toHexString(is.read())+"H ";
-	        		System.out.println(frame);
-	        		//Read first character from next frame 
-	        		currentCharacter = is.read();
+			{				
+	        	String frame = communication.readFrame();
+	        	
 	        		
 	        		//wait for the end of frame... some kind of check here
 	        		//...
 	        		//...
 	        		//when frame is complete
-	        		//notifyObservers( dataBuffer );
-	        	}
+	        		setChanged();
+	        		notifyObservers( frame );	        	
 			}
 			catch( Exception e )
 			{
