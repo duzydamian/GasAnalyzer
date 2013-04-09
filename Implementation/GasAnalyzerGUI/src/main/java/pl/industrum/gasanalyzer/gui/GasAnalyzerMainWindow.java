7a6136f29867e9afc2191d3ea435b86185427ffb
Damian Karbowiak <duzydamian@gmail.com>
2013-04-09 19:25:39 +0200

    Next stage of GUI. Added selection of forwarding System.out to window or file.

--- Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerMainWindow.java
+++ Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/gui/GasAnalyzerMainWindow.java
@@ -1,5 +1,10 @@
 package pl.industrum.gasanalyzer.gui;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.custom.StyledText;
@@ -16,7 +21,7 @@ import pl.industrum.gasanalyzer.elan.communication.ELANConnection;
 
 public class GasAnalyzerMainWindow {
 
-	protected Shell shell;	
+	protected Shell shlGasAnalyzer;	
 	
 	/**
 	 * GUI Element's 
@@ -24,10 +29,12 @@ public class GasAnalyzerMainWindow {
 	StyledText styledText;
 	Button connect;
 	CCombo portsList;
+	boolean outSelected;
 	
 
 	public GasAnalyzerMainWindow() {
 		super();
+		outSelected = false;
 	}
 	
 	/**
@@ -36,9 +43,9 @@ public class GasAnalyzerMainWindow {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlGasAnalyzer.open();
+		shlGasAnalyzer.layout();
+		while (!shlGasAnalyzer.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -50,12 +57,12 @@ public class GasAnalyzerMainWindow {
 	 * @wbp.parser.entryPoint
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlGasAnalyzer = new Shell();
+		shlGasAnalyzer.setSize(450, 300);
+		shlGasAnalyzer.setText("Gas Analyzer");
 		
-		Menu menu = new Menu(shell, SWT.BAR);
-		shell.setMenuBar(menu);
+		Menu menu = new Menu(shlGasAnalyzer, SWT.BAR);
+		shlGasAnalyzer.setMenuBar(menu);
 		
 		MenuItem mntmPlik = new MenuItem(menu, SWT.CASCADE);
 		mntmPlik.setText("Plik");
@@ -81,7 +88,7 @@ public class GasAnalyzerMainWindow {
 		MenuItem mntmO = new MenuItem(menu_3, SWT.NONE);
 		mntmO.setText("O programie");
 		
-		portsList = new CCombo(shell, SWT.BORDER);
+		portsList = new CCombo(shlGasAnalyzer, SWT.BORDER);
 		portsList.setBounds(46, 10, 85, 29);
 		
 		for (String port: ELANConnection.vectorPorts()) 
@@ -99,24 +106,25 @@ public class GasAnalyzerMainWindow {
 		}
 			
 
-		Label lblNewLabel = new Label(shell, SWT.NONE);
+		Label lblNewLabel = new Label(shlGasAnalyzer, SWT.NONE);
 		lblNewLabel.setBounds(10, 10, 30, 17);
 		lblNewLabel.setText("Port:");
-        //System.setErr();
-//        System.setOut(new PrintStream(new OutputStream() {
-//			
-//			@Override
-//			public void write(int arg0) throws IOException {				
-//				//if(arg0<100){
-//					byte[] character = new byte[1];
-//					character[0] = (byte) arg0;
-//					styledText.setText(styledText.getText()+new String(character));
-//				//}								
-//			}
-//		}));
 		
-		connect = new Button(shell, SWT.TOGGLE);
-		connect.setBounds(137, 10, 88, 29);
+		final Button btnOut = new Button(shlGasAnalyzer, SWT.RADIO);
+		btnOut.setBounds(232, 10, 50, 22);
+		btnOut.setText("out");
+		btnOut.setSelection(true);
+		
+		final Button btnOkno = new Button(shlGasAnalyzer, SWT.RADIO);
+		btnOkno.setBounds(288, 10, 63, 22);
+		btnOkno.setText("okno");
+		
+		final Button btnPlik = new Button(shlGasAnalyzer, SWT.RADIO);
+		btnPlik.setBounds(357, 10, 81, 22);
+		btnPlik.setText("plik");
+		
+		connect = new Button(shlGasAnalyzer, SWT.TOGGLE);
+		connect.setBounds(137, 10, 92, 29);
 		connect.setText("Połącz");
 		connect.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -124,7 +132,47 @@ public class GasAnalyzerMainWindow {
 				boolean state = connect.getSelection();
 		        try
 		        {
-		        	if(state)
+		        	if (!outSelected)
+		        	{
+		        		if (btnOut.getSelection())
+		        		{
+		        			
+		        		}
+		        		else if (btnOkno.getSelection())
+		        		{
+		        	        System.setOut(new PrintStream(new OutputStream()
+		        	        {	
+		        				@Override
+		        				public void write(final int arg0) throws IOException {
+		        					Display.getDefault().syncExec( 
+		        							new Runnable() 
+		        							{
+		        								public void run()
+		        								{
+		        									byte[] character = new byte[1];
+		        									character[0] = (byte) arg0;
+		        									styledText.setText(styledText.getText()+new String(character));
+		        								}
+		        							});										
+		        				}
+		        			}));
+		        		}
+		        		else if (btnPlik.getSelection())
+		        		{
+		        			try 
+		        			{
+		        				System.setErr(new PrintStream("err.log"));
+		        				System.setOut(new PrintStream("out.log"));
+		        			} 
+		        			catch (FileNotFoundException e1) 
+		        			{
+		        				e1.printStackTrace();
+		        			} 
+		        		}
+		        		outSelected = true;
+		        	}
+		        	
+		        	if (state)
 		        	{
 			        	ELANConnection.getInstance().connect(portsList.getItem(portsList.getSelectionIndex()));
 			        	portsList.setEnabled(false);
@@ -144,10 +192,8 @@ public class GasAnalyzerMainWindow {
 			}
 		});
 		
-		styledText = new StyledText(shell, SWT.BORDER);
-		styledText.setBounds(10, 45, 428, 186);
-						
-		ELANConnection.listPorts();
-        
+		styledText = new StyledText(shlGasAnalyzer, SWT.BORDER | SWT.V_SCROLL);		
+		styledText.setAlignment(SWT.CENTER);
+		styledText.setBounds(10, 45, 428, 186);		
 	}
 }
