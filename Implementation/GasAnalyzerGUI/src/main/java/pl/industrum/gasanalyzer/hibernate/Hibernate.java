3cc5c2b406269c6409f0f1944b639147ffba4c13
bananowygrzesiu <bananowygrzesiu@bananowygrzesiu-1201T>
2013-05-07 22:38:45 +0200

    *Change hibernate connection class name.

--- Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/hibernate/Hibernate.java
+++ Implementation/GasAnalyzerGUI/src/main/java/pl/industrum/gasanalyzer/hibernate/Hibernate.java
@@ -5,7 +5,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-public class HibernateUtil
+public class Hibernate
 {
 	private static final SessionFactory sessionFactory;
 	 
