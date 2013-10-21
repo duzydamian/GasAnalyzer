7dd8731d865e4458582e463395534f6bb40593c6
Damian Karbowiak <duzydamian@gmail.com>
2013-10-21 11:32:19 +0200

    First stage of analazer. Not complete, not tested.

--- Implementation/ELANNetworkAnalyzer/src/main/java/pl/industrum/gasanalyzer/analyzer/CollectData.java
+++ Implementation/ELANNetworkAnalyzer/src/main/java/pl/industrum/gasanalyzer/analyzer/CollectData.java
@@ -3,6 +3,17 @@
  */
 package pl.industrum.gasanalyzer.analyzer;
 
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import pl.industrum.gasanalyzer.elan.communication.ELANCommunication;
+import pl.industrum.gasanalyzer.elan.communication.ELANConnection;
+
 /**
  * @author duzydamian (Damian Karbowiak)
  *
@@ -10,21 +21,85 @@ package pl.industrum.gasanalyzer.analyzer;
 public class CollectData
 {
 
+	static boolean timeout;
 	/**
 	 * 
 	 */
 	public CollectData()
-	{
-		// TODO Auto-generated constructor stub
-	}
+	{ }
 
 	/**
 	 * @param args
 	 */
 	public static void main( String[] args )
 	{
-		// TODO Auto-generated method stub
+		Vector<String> vectorPortsOnlySerial = ELANConnection.vectorPortsOnlySerial();
+		int iterator = 0;
+		timeout = false;
+		Timer timer = new Timer("Reading time");
+		TimeIsUp timeIsUp = new TimeIsUp();
+				
+		for( String port: vectorPortsOnlySerial )
+		{
+			System.out.println( iterator + " : " + port );
+			iterator++;
+		}
+		
+		int value = 0;
+		
+		do
+		{
+			System.out.println( "Z którym urządzeniem połączyć ? [0-" + (vectorPortsOnlySerial.size()-1) + "]" );
+						
+			try
+			{
+				value = System.in.read() - 48;
+			} catch ( IOException e )
+			{
+				e.printStackTrace();
+			}
+		}
+		while ( !( value >= 0  && value <= vectorPortsOnlySerial.size()-1) );
+		
+		System.out.println( "Próbuję połączyć z " + vectorPortsOnlySerial.get( value ) );
+		
+		ELANConnection connection = new ELANConnection();
+		try
+		{
+			connection.connect( vectorPortsOnlySerial.get( value ),  new Observer()
+			{
+				
+				public void update( Observable o, Object arg )
+				{
+					// TODO Auto-generated method stub
+				}
+			});
+		}
+		catch ( Exception e )
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ELANCommunication communication = new ELANCommunication( connection );		
+		timer.schedule( timeIsUp, 50000 );
 
+		//int dataPart;
+		while(!timeout)
+		{
+			//dataPart = connection.read();
+			Queue<Integer> readFrame = communication.readFrame();
+			System.out.println( readFrame );
+		}
+		timer.cancel();
+		timeIsUp.cancel();
+		connection.disconnect();
 	}
-
+	
+	static class TimeIsUp extends TimerTask
+	{
+		public void run()
+		{
+			timeout = true;
+		}
+	}
 }
