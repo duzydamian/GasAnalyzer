4cbf36e02888f0f492211cb31666b62fc6ce538d
Damian Karbowiak <duzydamian@gmail.com>
2014-03-12 23:38:28 +0100

    First version of app which measure response time.

--- Implementation/ELANNetworkTimeAnalyzer/src/main/java/pl/industrum/gasanalyzer/timeanalyzer/TimeAnalyze.java
+++ Implementation/ELANNetworkTimeAnalyzer/src/main/java/pl/industrum/gasanalyzer/timeanalyzer/TimeAnalyze.java
@@ -3,12 +3,17 @@
  */
 package pl.industrum.gasanalyzer.timeanalyzer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Queue;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Vector;
 
 import pl.industrum.gasanalyzer.elan.communication.ELANCommunication;
@@ -20,8 +25,12 @@ import pl.industrum.gasanalyzer.elan.communication.ELANConnection;
  */
 public class TimeAnalyze
 {
-
+	private static SimpleDateFormat dateFormater = new SimpleDateFormat( "HH:mm:ss:SS dd/MM/yyyy", Locale.getDefault() );
+	
 	static boolean timeout;
+	public static File timeFile;
+	static BufferedWriter timeOutput;
+	
 	/**
 	 * 
 	 */
@@ -33,12 +42,27 @@ public class TimeAnalyze
 	 */
 	public static void main( String[] args )
 	{
+	try
+	{	
 		Vector<String> vectorPortsOnlySerial = ELANConnection.vectorPortsOnlySerial();
 		int iterator = 0;
 		timeout = false;
-		Timer timer = new Timer("Reading time");
-		TimeIsUp timeIsUp = new TimeIsUp();
-				
+		//Timer timer = new Timer("Reading time");
+		//TimeIsUp timeIsUp = new TimeIsUp();		
+					
+		timeFile = new File( "time.log" );	
+
+		timeOutput = new BufferedWriter(new FileWriter(timeFile));	
+		timeOutput.write( dateFormater.format( new Date() ) );		
+		timeOutput.newLine();
+		timeOutput.write( "------------------------------------------" );
+		timeOutput.newLine();
+		timeOutput.write( "					POMIARY					 " );
+		timeOutput.newLine();
+		timeOutput.write( "------------------------------------------" );
+		timeOutput.newLine();
+		timeOutput.flush();		
+		
 		for( String port: vectorPortsOnlySerial )
 		{
 			System.out.println( iterator + " : " + port );
@@ -81,25 +105,68 @@ public class TimeAnalyze
 			e.printStackTrace();
 		}
 		ELANCommunication communication = new ELANCommunication( connection );		
-		timer.schedule( timeIsUp, 50000 );
+		//timer.schedule( timeIsUp, 50000 );
 
 		//int dataPart;
-		while(!timeout)
+		//while(!timeout)
+		Queue<Integer> writeFrame = new LinkedList<Integer>();
+		writeFrame.add( 0x10 );
+		writeFrame.add( 0x01 );
+		writeFrame.add( 0xA0 );
+		writeFrame.add( 0xE0 );
+		writeFrame.add( 0x6B );
+		//writeFrame.add( 0x06 ); //device info
+		writeFrame.add( 0x02 ); //pomiary
+		writeFrame.add( 0x10 );
+		writeFrame.add( 0x03 );
+		//writeFrame.add( 0x79 ); //device info
+		//writeFrame.add( 0x55 ); //device info
+		writeFrame.add( 0x38 ); //pomiary
+		writeFrame.add( 0x94 ); //pomiary
+		
+		int  previousCharacter = -1;
+    	int  curentCharacter = -1;
+		
+		for ( int i = 0; i < 3; i++ )
 		{
-			//dataPart = connection.read();
-			Queue<Integer> readFrame = communication.readFrame();
-			System.out.println( readFrame );
+			communication.writeFrame( writeFrame );
+			long nanoTime = System.nanoTime();			
+			long currentTimeMillis = System.currentTimeMillis();			
+			curentCharacter = connection.read();
+			long nanoTime2 = System.nanoTime();
+			long currentTimeMillis2 = System.currentTimeMillis();						
+			do
+    		{   
+    			previousCharacter = curentCharacter;  
+    			//Read next character from new frame
+    			curentCharacter = connection.read();	
+    			//Add current character to collected frame    			
+    		}
+    		while ( !( (previousCharacter==16) & (curentCharacter==3) ) );
+			long nanoDiff = nanoTime2 - nanoTime;
+			long miliDiff = currentTimeMillis2 - currentTimeMillis;
+			timeOutput.write( "Czas nano: " + nanoDiff );
+			timeOutput.write( "Czas nano: " + miliDiff );
+			System.out.println( "Czas nano: " + nanoDiff );
+			System.out.println( "Czas mili: " + miliDiff );
 		}
-		timer.cancel();
-		timeIsUp.cancel();
+		timeOutput.write( "------------------------------------------" );
+		timeOutput.newLine();
+		//timer.cancel();
+		//timeIsUp.cancel();
 		connection.disconnect();
 	}
+	catch ( IOException e1 )
+	{
+		e1.printStackTrace();
+	}		
+	}
 	
-	static class TimeIsUp extends TimerTask
+/*	static class TimeIsUp extends TimerTask
 	{
 		public void run()
 		{
 			timeout = true;
 		}
-	}
+	}*/
 }
